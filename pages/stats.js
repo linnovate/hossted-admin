8490bf302830acd40866547fb0a4b6804663268c
Lior Kesos <lior@linnovate.net>
2021-04-18 23:24:45 +0300

    Adding stats

---
+++ pages/stats.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout'
+
+export default function Page () {
+  return (
+    <Layout>
+      <h1>Amazon Stats page</h1>
+      <p>
+        This is for amazon stats.
+      </p>
+    </Layout>
+  )
+}
