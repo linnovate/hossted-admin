fdbc71169387a0698dcce357f05525127de4702f
Lior Kesos <lior@linnovate.net>
2021-04-19 00:56:14 +0300

    Adding awsget

---
+++ pages/api/aws.js
@@ -0,0 +1,17 @@
+// This is an example of to protect an API route
+import { getSession } from 'next-auth/client'
+
+export default async (req, res) => {
+  const session = await getSession({ req })
+  
+  if (session){
+    const domain = session.user.email.split('@')[0];
+    if (domain === 'linnovate.net') {
+      res.send({ content: 'This is protected content. You can access this content because you are signed in.' })
+    } else {
+      res.send({ error: domain + 'is not allowed to connect' })
+    }
+  } else {
+    res.send({ error: 'Sorry no anonoymous or unauthorised access allowed to this page'})
+  }
+}
