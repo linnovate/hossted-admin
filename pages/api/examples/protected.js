14545567019e07fd6adfcc7d807250ffce7263bb
Lior Kesos <lior@linnovate.net>
2021-04-18 23:50:22 +0300

    more domainbased assertions

--- pages/api/examples/protected.js
+++ pages/api/examples/protected.js
@@ -3,10 +3,14 @@ import { getSession } from 'next-auth/client'
 
 export default async (req, res) => {
   const session = await getSession({ req })
-  console.log(session.user);
-  if (session) {
-    res.send({ content: 'This is protected content. You can access this content because you are signed in.' })
-  } else {
-    res.send({ error: 'You must be sign in to view the protected content on this page.' })
-  }
-}
+  const domain = session.user.email.split('@')[0];
+  
+  if (domain){
+    if (domain === 'linnovate.net') {
+      res.send({ content: 'This is protected content. You can access this content because you are signed in.' })
+    } else {
+      res.send({ error: domain + 'is not allowed to connect' })
+    }
+  }else {
+    res.send('Sorry no anonoymous or unauthorised access allowed to this page')
+  } 
