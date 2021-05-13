4fc941d6a0e820f7edb17b5fa467762f506f57c4
Lior Kesos <lior@linnovate.net>
2021-05-13 10:48:01 +0300

    Fixing syntax problems

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,11 +1,11 @@
 
 export default async (req, res) => {
     console.log(req.body);
-    console.log(req.protocol)     // "https"
-    console.log(req.hostname)     // "example.com"
-    console.log(req.path)         // "/creatures"
-    console.log(req.originalUrl)  // "/creatures?filter=sharks"
-    console.log(req.subdomains)   // "['ocean']"
+    console.log(req.protocol);     // "https"
+    console.log(req.hostname);     // "example.com"
+    console.log(req.path) ;        // "/creatures"
+    console.log(req.originalUrl);  // "/creatures?filter=sharks"
+    console.log(req.subdomains);  // "['ocean']"
    
     res.send('kookoo');   
-})
+}
