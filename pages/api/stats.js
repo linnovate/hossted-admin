d516bd00076626a55fd3732565c2089264a797f4
Lior Kesos <lior@linnovate.net>
2021-05-13 10:55:51 +0300

    more generic thingy

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,11 +1,5 @@
 
-export default async (req, res) => {
-    console.log(req.body);
-    console.log(req.protocol);     // "https"
-    console.log(req.hostname);     // "example.com"
-    console.log(req.path) ;        // "/creatures"
-    console.log(req.originalUrl);  // "/creatures?filter=sharks"
-    console.log(req.subdomains);  // "['ocean']"
-   
-    res.send('kookoo');   
+export default function handler(req, res) {
+  const { pid } = req.query
+  res.end(`Post: ${pid}`)
 }
