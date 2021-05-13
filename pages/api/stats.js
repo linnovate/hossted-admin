403f9ddad17286e611806b1ba5fccb8110af571a
Lior Kesos <lior@linnovate.net>
2021-05-13 10:44:48 +0300

    more elaborate reqs

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,6 +1,11 @@
 
 export default async (req, res) => {
-    console.log(JSON.stringify(req));
+    console.log(JSON.stringify(req.body));
+    console.log(req.protocol)     // "https"
+    console.log(req.hostname)     // "example.com"
+    console.log(req.path)         // "/creatures"
+    console.log(req.originalUrl)  // "/creatures?filter=sharks"
+    console.log(req.subdomains)   // "['ocean']"
    
     res.send('kookoo');   
 })
