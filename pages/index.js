129557af2bd1aaa359c07985f08ef80133882761
Cheskel Twersky <twerskycheskel@gmail.com>
2021-09-30 09:20:13 +0300

    update graphs

--- pages/index.js
+++ pages/index.js
@@ -5,79 +5,79 @@ import { useState, useEffect } from 'react'
 import AccessDenied from '../components/access-denied'
 
 
-const ChartWithNoSSR = dynamic(() => import('react-apexcharts'), {
-    ssr: false,
-  });
+// const ChartWithNoSSR = dynamic(() => import('react-apexcharts'), {
+//     ssr: false,
+//   });
 
-const ApexChart = (props) => {
-return <ChartWithNoSSR {...props} />;
-};
+// const ApexChart = (props) => {
+// return <ChartWithNoSSR {...props} />;
+// };
 
 
 
 
 export default function Page () {
   const [ session, loading ] = useSession()
-  const [ graphData, setGraphData] = useState({
-      series: [],
-      options: {}
-  })
+  // const [ graphData, setGraphData] = useState({
+  //     series: [],
+  //     options: {}
+  // })
 
-  useEffect(()=>{
-    const fetchData = async () => {
-      const res = await fetch('/api/data/get_overall_graph')
-      const json = await res.json()
-      let data = {}
-      data.series = json.series
-      data.options = {
-          chart: {
-              height: 420,
-              type: 'line',
-              dropShadow: {
-                  enabled: true,
-                  color: '#000',
-                  top: 18,
-                  left: 7,
-                  blur: 10,
-                  opacity: 0.2,
-              },
-              toolbar: {
-                  show: false,
-              },
-          },
-          colors: ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#000000'],
-          dataLabels: {
-              enabled: true,
-          },
-          stroke: {
-              curve: 'smooth',
-          },
-          title: {
-              text: 'Hours overall',
-              align: 'left',
-          },
-          markers: {
-              size: 1,
-          },
-          xaxis: {
-              categories: json.months,
-              title: {
-                  text: 'Month',
-              },
-          },
-          yaxis: {
-              title: {
-                  text: 'Hours',
-              },
-          },
-          legend: {
-              show: false
-          },
-      }
-      setGraphData(data)
-    }
-    fetchData()
-  },[session])
+  // useEffect(()=>{
+  //   const fetchData = async () => {
+  //     const res = await fetch('/api/data/get_overall_graph')
+  //     const json = await res.json()
+  //     let data = {}
+  //     data.series = json.series
+  //     data.options = {
+  //         chart: {
+  //             height: 420,
+  //             type: 'line',
+  //             dropShadow: {
+  //                 enabled: true,
+  //                 color: '#000',
+  //                 top: 18,
+  //                 left: 7,
+  //                 blur: 10,
+  //                 opacity: 0.2,
+  //             },
+  //             toolbar: {
+  //                 show: false,
+  //             },
+  //         },
+  //         colors: ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#000000'],
+  //         dataLabels: {
+  //             enabled: true,
+  //         },
+  //         stroke: {
+  //             curve: 'smooth',
+  //         },
+  //         title: {
+  //             text: 'Hours overall',
+  //             align: 'left',
+  //         },
+  //         markers: {
+  //             size: 1,
+  //         },
+  //         xaxis: {
+  //             categories: json.months,
+  //             title: {
+  //                 text: 'Month',
+  //             },
+  //         },
+  //         yaxis: {
+  //             title: {
+  //                 text: 'Hours',
+  //             },
+  //         },
+  //         legend: {
+  //             show: false
+  //         },
+  //     }
+  //     setGraphData(data)
+  //   }
+  //   fetchData()
+  // },[session])
 
   // When rendering client side don't display anything until loading is complete
   if (typeof window !== 'undefined' && loading) return null
@@ -92,12 +92,18 @@ export default function Page () {
 
   return (
     <Layout>
-      <ApexChart
+      {/* <ApexChart
       options={graphData.options}
       series={graphData.series}
       type="line"
       height={420}
-      />
+      /> */}
+    <p>Usage over time</p>
+    <p>Aws</p>
+    <iframe className='kibana-iframe-half' src="https://kibana-14.dev.aks.linnovate.net/app/visualize#/edit/edaa5c20-1155-11ec-99da-bb9b9b878429?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))"></iframe>
+    <p>Azure</p>
+    <iframe className='kibana-iframe-half' src="https://kibana-14.dev.aks.linnovate.net/app/visualize#/edit/f56bed80-13a7-11ec-99da-bb9b9b878429?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))"></iframe>
+
   </Layout>
   )
 }
